21ab7c14fe2feae958fc78a4d6695f02c7d84582
ayushraghuwanshi7500 <66081125+ayushraghuwanshi7500@users.noreply.github.com>
2021-04-14 12:51:45 +0530

    style

--- src/components/PredictionResult.jsx
+++ src/components/PredictionResult.jsx
@@ -5,34 +5,33 @@ const PredictionResult = ({ formData, setFormData }) => {
   window.onload = function () {
     window.location = '/';
   };
-  const mystyle = {
-    textAlign: 'center',
-    Rowor: '#171717',
-    fontSize: 15,
-    marginBottom: 5
-  };
   console.log(formData);
   const { city, postalCode, category, brand } = formData;
-  const query = [city, postalCode, category, brand];
-  return (
-    <div style={{ color: 'black' }}>
-      <List
-        size='small'
-        style={{ textAlign: 'center' }}
-        header={
-          <h6 style={{ marginTop: 5, textAlign: 'center' }}>Your Query</h6>
-        }
-        bordered
-        dataSource={query}
-        renderItem={(item) => (
-          <List.Item style={{ textAlign: 'center' }}>{item}</List.Item>
-        )}
-      />
+  const query = [city, postalCode, brand, category];
 
-      <Link to='/'>
-        <Button>Predict Again</Button>
-      </Link>
-    </div>
+  return (
+    <>
+      <Row gutter={24} style={{ margin: 5 }}>
+        <Col className='gutter-row' span={4}>
+          <List
+            size='small'
+            style={{ textAlign: 'center' }}
+            header={
+              <h6 style={{ marginTop: 5, textAlign: 'center' }}>Your Query</h6>
+            }
+            bordered
+            dataSource={query}
+            renderItem={(item) => (
+              <List.Item style={{ textAlign: 'center' }}>{item}</List.Item>
+            )}
+          />
+
+          <Link to='/'>
+            <Button style={{ width: '100%' }}>Predict Again</Button>
+          </Link>
+        </Col>
+      </Row>
+    </>
   );
 };
 
