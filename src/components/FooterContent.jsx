947c7a8b5b678cc37c5efdfcb2d61e70e6b15394
ayushraghuwanshi7500 <66081125+ayushraghuwanshi7500@users.noreply.github.com>
2022-09-09 22:17:32 +0530

    commit

--- src/components/FooterContent.jsx
+++ src/components/FooterContent.jsx
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from 'react';
-import '../index.css';
-=======
-=======
->>>>>>> parent of 154e064 (ccl-project-changes)
+
 import {
   AreaChartOutlined,
   ClearOutlined,
@@ -12,108 +6,11 @@ import {
   DesktopOutlined
 } from "@ant-design/icons";
 import { Steps } from "antd";
-import React from "react";
-<<<<<<< HEAD
->>>>>>> parent of 154e064 (ccl-project-changes)
-=======
->>>>>>> parent of 154e064 (ccl-project-changes)
+import React from 'react';
+import '../index.css';
 const FooterContent = () => {
-  const cursorStyle = { cursor: 'pointer' };
-  const LINKED_IN_LINK = 'https://www.linkedin.com/in/';
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-      }}
-    >
-      <div
-        className='namestyle'
-        onClick={() =>
-          window.open(`${LINKED_IN_LINK}gauri-patil-17a0131b3`, '_blank')
-        }
-        style={cursorStyle}
-      >
-        Gauri Patil
-      </div>
-      <div
-        style={{
-          height: '8px',
-          width: '8px',
-          backgroundColor: '#000',
-          borderRadius: '50%'
-        }}
-      />
-      <div
-        onClick={() =>
-          window.open(`${LINKED_IN_LINK}ayush-raghuwanshi-333a1a1a2`, '_blank')
-        }
-        className='namestyle'
-        style={cursorStyle}
-      >
-        Ayush Raghuwanshi
-      </div>
-      <div
-        style={{
-          height: '8px',
-          width: '8px',
-          backgroundColor: '#000',
-          borderRadius: '50%'
-        }}
-      />
-      <div
-        onClick={() =>
-          window.open(`${LINKED_IN_LINK}yukta-thakur-39b8b1199`, '_blank')
-        }
-        className='namestyle'
-        style={cursorStyle}
-      >
-        Yukta Thakur
-      </div>
-      <div
-        style={{
-          height: '8px',
-          width: '8px',
-          backgroundColor: '#000',
-          borderRadius: '50%'
-        }}
-      />
-      <div
-        onClick={() =>
-          window.open(`${LINKED_IN_LINK}simran-kalaskar-51847617a`, '_blank')
-        }
-        className='namestyle'
-        style={cursorStyle}
-      >
-        Simran Kalaskar
-      </div>
-      <div
-        style={{
-          height: '8px',
-          width: '8px',
-          backgroundColor: '#000',
-          borderRadius: '50%'
-        }}
-      />
-      <div style={{ color: '#000', fontSize: '20px' }}>&#169; 2022</div>
-      <div
-        style={{
-          height: '8px',
-          width: '8px',
-          backgroundColor: '#000',
-          borderRadius: '50%'
-        }}
-      />
-      <div style={{ color: '#000', fontSize: '20px' }}>
-        Shopper Footfall Prediction
-      </div>
-    </div>
-=======
-=======
->>>>>>> parent of 154e064 (ccl-project-changes)
+
     <>
       <Steps size="small" current={4}>
         <Steps.Step
@@ -185,11 +82,7 @@ const FooterContent = () => {
         />
       </Steps>
     </>
-<<<<<<< HEAD
->>>>>>> parent of 154e064 (ccl-project-changes)
-=======
->>>>>>> parent of 154e064 (ccl-project-changes)
-  );
+  )
 };
 
 export default FooterContent;
